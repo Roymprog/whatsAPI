4aa73d3bf13d49ccd773575bbfa9bc31044724e9
Roymprog <roy_van_santen@hotmail.com>
2017-11-08 18:04:20 +0100

    Add authorization to post route

--- routes/message.js
+++ routes/message.js
@@ -2,9 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* Respond to webhooks from API. */
-router.post('/in', (req, res) => {
-  console.log(req);
-  res.send('Received');
+router.post('/in', (req, res, next) => {
+  if (req.token !== 'abcd1234') {
+    const err = new Error('You do not have the rights to do this!');
+    error.code = 'E_UNAUTHORIZED'
+    err.httpStatusCode = 403;
+    console.log(err);
+    return next(err);
+  }
+  console.log(req.body);
+  return res.send('Received');
 });
 
 module.exports = router;

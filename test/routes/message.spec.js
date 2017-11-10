a8d3500eb41c8e80e3f0458c4694abd97ed89253
Roymprog <roy_van_santen@hotmail.com>
2017-11-10 15:51:22 +0100

    Add test for incoming message token

--- test/routes/message.spec.js
+++ test/routes/message.spec.js
@@ -1,5 +1,14 @@
 const assert = require('chai').assert;
 
-const {isTokenSet} = require('/home/fd99od/personal_web_projects/whatsAPI/routes/message.js');
+const { isTokenSet } = require('/home/fd99od/personal_web_projects/whatsAPI/routes/message.js');
 
-assert.isFalse(isTokenSet('a'));
+describe('Messages', () => {
+    describe('incoming: ', () => {
+        it('should return false when wrong token is set', () => {
+            assert.isFalse(isTokenSet('a'));
+        });
+        it('should return true when wrong token is set', () => {
+            assert.isTrue(isTokenSet('abcd1234'));
+        });
+    });
+});

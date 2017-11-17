ba05a5e2b9b41bafe2720dbbae4c96ac355a9c87
Roymprog <roy_van_santen@hotmail.com>
2017-11-17 07:51:36 +0100

    Fix(): clean up

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ app.use((req, res, next) => {
 // error handler
 app.use((err, req, res, next) => {
   // // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.httpStatusCode || 500);
   return res.send(err);
 });
 
--- routes/message.js
+++ routes/message.js
@@ -1,19 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-const {isTokenSet} = require('../lib/validations');
+const { isTokenSet } = require('../lib/validations');
 
 /* Respond to webhooks from API. */
 router.post('/in', (req, res, next) => {
-  if (isTokenSet(req.body.token)) {
-    const err = new Error('You do not have the rights to do this!');
-    error.code = 'E_UNAUTHORIZED'
-    err.httpStatusCode = 403;
-    console.log(err);
-    return next(err);
-  }
-  console.log(req.body);
-  return res.send('Received');
+  isTokenSet(req.body.token).then(() => {
+    return res.send('Received');
+  }).catch(err => 
+    next(err)
+  );
 });
 
 module.exports = router;

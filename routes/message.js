a112e1c528835f28c836a0573223bf23f8f4b008
Roymprog <roy_van_santen@hotmail.com>
2017-11-17 08:24:08 +0100

    Check for message type test pass

--- routes/message.js
+++ routes/message.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-const { isTokenSet } = require('../lib/validations');
+const { isTokenSet, isTextMessage } = require('../lib/validations');
 
 /* Respond to webhooks from API. */
 router.post('/in', (req, res, next) => {
-  isTokenSet(req.body.token).then(() => {
+  const message = req.body;
+  isTokenSet(message.token)
+  .then(isTextMessage(message.type))
+  .then(() => {
     return res.send('Received');
   }).catch(err => 
     next(err)

4906a7a4db6c1b1b8e150f48dce3a6a5d23c669c
Roymprog <roy_van_santen@hotmail.com>
2017-11-25 22:40:37 +0100

    Add message model

--- models/message.js
+++ models/message.js
@@ -1,5 +1,10 @@
-class message {
-  constructor(phoneNumber, text) {
-    
+class Message {
+  constructor({ receiverNumber, senderNumber, text, direction, type }) {
+    this.receiverNumber = receiverNumber;
+    this.senderNumber = senderNumber;
+    this.text = text;
+    this.direction = direction;
   }
 }
+
+module.exports = { Message }
--- routes/message.js
+++ routes/message.js
@@ -2,13 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 const { isTokenSet, isTextMessage } = require('../lib/validations');
+const Message = require('../models/message');
 
 /* Respond to webhooks from API. */
 router.post('/in', (req, res, next) => {
-  const message = req.body;
+  const message = new Message({
+    receiverNumber: 31657594215,
+    senderNumber: req.body.uid,
+    text: req.body.message.body.type,
+    direction: "incoming",
+    type: req.body.message.type,
+    acknowledgementStatus: req.body.ack,
+  });
+  console.log('message:', message)
   isTokenSet(message.token)
   .then(isTextMessage)
   .then((message) => {
+    
     return res.send('Received');
   }).catch(err => {
     return next(err)}
